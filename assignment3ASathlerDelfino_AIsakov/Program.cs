c99bf32bdaa2a599b55741dbf46edc9d2a4127fa
Aline Sathler <sathler@ymail.com>
2023-10-30 21:14:09 -0400

    Adding tie Break

--- assignment3ASathlerDelfino_AIsakov/Program.cs
+++ assignment3ASathlerDelfino_AIsakov/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,13 @@ using System.Threading.Tasks;
 //Purpose: C# console application of a tournament of Rock, Paper, Scissors
 //Revision History:
 //REV00 - 2023/10/26 - Initial version
+//REV01 - 2023/10/30 - Adding tie Break
 
 namespace assignment3ASathlerDelfino_AIsakov {
+    //Global Variables
+    public static class Globals {
+        public static uint playerWins = 0, computerWins = 0;
+    }
     internal class Program {
         
 
@@ -36,7 +42,7 @@ namespace assignment3ASathlerDelfino_AIsakov {
 
         }
 
-        //Commputer draw is selected randomly
+        //Computer draw is selected randomly
         static uint ComputerPlays() {
             Random rand = new Random();
 
@@ -61,8 +67,8 @@ namespace assignment3ASathlerDelfino_AIsakov {
             return prompt;
         }
 
-        //Checking who won the game
-        static string CheckingWinner(uint playerChoice, uint computerChoice) {
+        //Checking winner game
+        static string WinnerGame(uint playerChoice, uint computerChoice) {
             string winner = "";
             if (playerChoice == computerChoice) {
                 winner = "DRAW!";
@@ -71,13 +77,73 @@ namespace assignment3ASathlerDelfino_AIsakov {
             } else if ((playerChoice == 2 && computerChoice == 1) || (playerChoice == 1 && computerChoice == 3) || (playerChoice == 3 && computerChoice == 2)) {
                 winner = "Player Wins";
             }
-
             return winner;
         }
-        static void Main() {
-            uint games, computerChoice, playerChoice, playerWins = 0, computerWins = 0;
+
+        //Checking winner series
+        static void WinnerSeries () {
+            //Showing the winner of the series
+            if (Globals.playerWins > Globals.computerWins) {
+                Console.WriteLine("\nCongrats Player Wins the Series!!\n");
+            } else if (Globals.playerWins < Globals.computerWins) {
+                Console.WriteLine("\nComputer Wins the Series.\n");
+            } else {
+                Console.WriteLine("\nSeries ended in a draw.");
+                //Tie Breaker game
+                TieBreaker();
+            }
+        }
+
+        
+        //Game
+        static void Game() {
+            uint computerChoice, playerChoice;
             string winner;
 
+            playerChoice = PlayerPlays();
+            computerChoice = ComputerPlays();
+
+            Console.WriteLine("Rock, Paper, Scissors, DRAW!");
+
+            //Writing the opponents choices on terminal
+            Console.WriteLine(WriteChoices("Player", playerChoice));
+            Console.WriteLine(WriteChoices("Computer", computerChoice));
+
+            //Checking who won the game
+            winner = WinnerGame(playerChoice, computerChoice);
+            Console.WriteLine(winner);
+
+            //Adding the win for the winner
+            if (winner == "Player Wins") {
+                Globals.playerWins++;
+            } else if (winner == "Computer Wins") {
+                Globals.computerWins++;
+            }
+
+            //Displaying the result of consecutive games
+            Console.WriteLine("\nStandings:");
+            Console.WriteLine("Player Wins: " + Globals.playerWins);
+            Console.WriteLine("Computer Wins: " + Globals.computerWins);
+        }
+
+        //Tie Breaker
+        static void TieBreaker() {
+            bool isDraw = true;
+            do {
+                Console.WriteLine("\n\nTIE BREAKER");
+                Game();
+
+                if (Globals.playerWins != Globals.computerWins) {
+                    isDraw = false;
+                }
+            } while (isDraw);
+
+            WinnerSeries();
+        }
+
+        static void Main() {
+            uint games;
+            
             Console.WriteLine("Welcome to Rock, Paper, Scissors Tournament!");
             Console.WriteLine("How many games would you like to play?");
 
@@ -90,40 +156,12 @@ namespace assignment3ASathlerDelfino_AIsakov {
                 //Loop for running for the quantity of games
                 for (int i = 0; i < games; i++)
                 {
-                    playerChoice = PlayerPlays();
-                    computerChoice = ComputerPlays();
-
-                    Console.WriteLine("Rock, Paper, Scissors, DRAW!");
-
-                    //Writing the opponents choices on terminal
-                    Console.WriteLine(WriteChoices("Player", playerChoice));
-                    Console.WriteLine(WriteChoices("Computer", computerChoice));
-
-                    //Checking who won the game
-                    winner = CheckingWinner(playerChoice, computerChoice);
-                    Console.WriteLine(winner);
-
-                    //Adding the win for the winner
-                    if (winner == "Player Wins") {
-                        playerWins++;
-                    } else if (winner == "Computer Wins") {
-                        computerWins++;
-                    }
-
-                    //Displaying the result of consecutive games
-                    Console.WriteLine("\nStandings:");
-                    Console.WriteLine("Player Wins: " + playerWins);
-                    Console.WriteLine("Computer Wins: " + computerWins);
+                    //Method that controls the game
+                    Game();
                 }
 
-                //Showing the winner of the series
-                if (playerWins > computerWins) {
-                    Console.WriteLine("\nCongrats Player Wins the Series!!");
-                } else if (playerWins < computerWins) {
-                    Console.WriteLine("\nComputer Wins the Series.");
-                } else {
-                    Console.WriteLine("\nSeries ended in a draw.");
-                }
+                //Checking who wins the series
+                WinnerSeries();
 
             } catch (FormatException) {
                 Console.WriteLine("Invalid input.");
